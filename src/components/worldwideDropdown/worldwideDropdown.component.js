f873eccd0e6b3d539a17497b621e51619aabdfc3
Muneeb Muhammad <muneebkattak57@gmail.com>
2023-11-20 08:54:48 +0500

    added sub search bar to Worldwied component

--- src/components/worldwideDropdown/worldwideDropdown.component.js
+++ src/components/worldwideDropdown/worldwideDropdown.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Dropdown from 'react-multilevel-dropdown';
-import { DropdownIcon } from './worldwideDropdown.styles';
+import { DropdownIcon, SearchBarContainer, SearchIcon, SearchInput } from './worldwideDropdown.styles';
 import Flag from 'react-world-flags'
 
 
@@ -8,9 +8,18 @@ const CountryCheckbox = ({country, code}) =>{
     return (
         <div>
             <input type="checkbox" style={{marginLeft: '5px', marginRight: '5px', backgroundColor: 'red'}}/>
-            <Flag code={code} style={{ width: '15px', height: '15px', borderRadius: '100%', marginRight: '5px' }}/>
+            <Flag code={code} style={{ width: '15px', height: '15px', borderRadius: '0%', marginRight: '5px', paddingTop: '3px'}}/>
             {country}
         </div>
+    )
+}
+
+const RegionSearchBar = () =>{
+    return (
+        <SearchBarContainer>
+            <SearchIcon src="/search-btn-purple.svg"/>
+            <SearchInput placeholder="Search regions / Countries" />
+        </SearchBarContainer>
     )
 }
 
@@ -46,40 +55,88 @@ const WorldwideDropdown = () => {
                     <DropdownIcon src="/dropdown-arrow-svgrepo-com.svg" />
                 </div>
             } style={{backgroundColor: '#E8DEFE', color: '#6937F2'}} position='right'>
-
-                <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    Africa <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                <Dropdown.Item>
+                    <div><RegionSearchBar /></div>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <span style={{fontSize: '12px', opacity: '0.7'}}>Regions</span>
+                </Dropdown.Item>
+                <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>Africa</span>
+                    </div>
+                    </div>
+                     
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                        <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>Africa</span>
                         {renderCountryCheckboxes('Africa', 'AfricaCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
                 <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    Asia <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>Asia</span>
+                    </div>
+                    </div>
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                    <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>Asia</span>
                         {renderCountryCheckboxes('Asia', 'AsiaCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
                 <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    Australia <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>Australia</span>
+                    </div>
+                    </div>
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                    <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>Australia</span>
                         {renderCountryCheckboxes('Australia', 'AustraliaCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
                 <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    Europe <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>Europe</span>
+                    </div>
+                    </div>
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                    <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>Europe</span>
                         {renderCountryCheckboxes('Europe', 'EuropeCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
                 <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    North America <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>North America</span>
+                    </div>
+                    </div>
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                    <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>North America</span>
                     {renderCountryCheckboxes('NorthAmerica', 'NorthAmericaCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
                 <Dropdown.Item style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    South America <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
-                    <Dropdown.Submenu position='right'>
+                    <div>
+                    <div>
+                        <input type='checkbox'></input>
+                        <span style={{paddingLeft: '10px'}}>South America</span>
+                    </div>
+                    </div>
+                    <DropdownIcon src='/dropdown-arrow-svgrepo-com.svg' style={{ transform: 'rotate(-90deg)' }} />
+                    <Dropdown.Submenu position='right' style={{color: 'black'}}>
+                    <span style={{paddingLeft: '5px', paddingBottom: '10px', display: 'inline-block', opacity: '0.7', fontSize: '12px'}}>South America</span>
                     {renderCountryCheckboxes('SouthAmerica', 'SouthAmericaCode')}
                     </Dropdown.Submenu>
                 </Dropdown.Item>
--- src/components/worldwideDropdown/worldwideDropdown.styles.js
+++ src/components/worldwideDropdown/worldwideDropdown.styles.js
@@ -10,7 +10,45 @@ export const Dropdown = styled.select`
 `;
 
 export const DropdownIcon = styled.img`
-    height: 2.5rem;
-    width: 2.5rem;
+    height: 2rem;
+    width: 2rem;
     object-fit: cover;
+    padding-left: 10px
+  `
+
+export const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #FFF;
+  padding: 5px 20px;
+  border-radius: 5px;
+  min-width: 0;
+  flex-wrap: nowrap;
+  border: 1px solid rgba(128, 128, 128, 0.3);
+`;
+
+export const SearchInput = styled.input`
+  flex-grow: 1;
+  padding: 5px 5px;
+  border: none;
+  border-radius: 5px;
+  color: black;
+  background-color: transparent;
+  width: 170px;
+  &::placeholder {
+    color: black;
+    opacity: 0.4; // Adjust as needed
+  }
+  &:focus {
+    outline: none;
+  }
+  
+`;
+
+export const SearchIcon = styled.img`
+    height: 2rem;
+    width: 2rem;
+    object-fit: cover;
+    filter: brightness(0);
+    opacity: 0.3
   `
