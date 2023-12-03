90c42babf003aa399329246787dddbc821a28512
moezAhmad <moez.ahmad.dev@gmail.com>
2023-12-04 02:07:56 +0500

    Implemented Edit and delete searches

--- src/components/MySearchesItem/MySearchesItem.component.js
+++ src/components/MySearchesItem/MySearchesItem.component.js
@@ -1,24 +1,86 @@
-import React from "react";
-import { ItemContainer, ItemDate, ItemHeader, ItemTitle, HashtagContainer, Hashtag } from "./MySearchesItem.styles";
+import React, { useState } from "react";
+import {
+  ItemContainer,
+  ItemDate,
+  ItemHeader,
+  ItemTitle,
+  HashtagContainer,
+  Hashtag,
+  EditTextarea,
+  RenameButton,
+  UpdateContainer,
+  CancelButton,
+  DeleteButton,
+} from "./MySearchesItem.styles";
 
-const MySearchesItem = ({item})=>{
-    return (
-        <ItemContainer>
-          <ItemHeader>
-            <ItemTitle>{item.name}</ItemTitle>
-            edit | delete
-          </ItemHeader>
-          <ItemHeader>
-            <HashtagContainer>
-                {item.hashtags.map((hashtag, index) => (
-                <Hashtag key={index}>{hashtag}</Hashtag>
-                ))}
-            </HashtagContainer>
-            <ItemDate>{item.date}</ItemDate>
-          </ItemHeader>
-          
-        </ItemContainer>
-      );
-}
+const MySearchesItem = ({ item, handleEditSearch, handleDeleteSearch }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [newName, setNewName] = useState(item.name);
+  return (
+    <ItemContainer>
+      {!isEditing && !isDeleting && (
+        <ItemHeader>
+          <ItemTitle>{item.name}</ItemTitle>
+          <div>
+            <img
+              src="/edit.svg"
+              onClick={() => setIsEditing(true)}
+              style={{ marginRight: "5px" }}
+            />
+            <img src="/trash-2.svg" onClick={() => setIsDeleting(true)} />
+          </div>
+        </ItemHeader>
+      )}
+      {!isEditing && !isDeleting && (
+        <ItemHeader>
+          <HashtagContainer>
+            {item.hashtags.map((hashtag, index) => (
+              <Hashtag key={index}>{hashtag}</Hashtag>
+            ))}
+          </HashtagContainer>
+          <ItemDate>{item.date}</ItemDate>
+        </ItemHeader>
+      )}
 
-export default MySearchesItem
+      {isEditing && (
+        <div>
+          <EditTextarea
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <UpdateContainer>
+            <RenameButton
+              onClick={() => {
+                handleEditSearch(item.id, newName);
+                setIsEditing(false);
+              }}
+            >
+              Rename
+            </RenameButton>
+          </UpdateContainer>
+        </div>
+      )}
+      {isDeleting && (
+        <div>
+          <EditTextarea value={"Are you sure to delete query"} readOnly />
+          <UpdateContainer>
+            <CancelButton onClick={() => setIsDeleting(false)}>
+              Cancel
+            </CancelButton>
+            <DeleteButton
+              onClick={() => {
+                handleDeleteSearch(item.id);
+                setIsDeleting(false);
+              }}
+            >
+              Delete
+            </DeleteButton>
+          </UpdateContainer>
+        </div>
+      )}
+    </ItemContainer>
+  );
+};
+
+export default MySearchesItem;
--- src/components/mySearchesModal/MySearchesModal.component.js
+++ src/components/mySearchesModal/MySearchesModal.component.js
@@ -1,66 +1,124 @@
-import React, {useState} from "react";
-import Modal from 'react-bootstrap/Modal';
+import React, { useEffect, useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import Dropdown from "react-multilevel-dropdown";
-import { ModalDialog, ModalHeader, ModalFooter, ModalBody, DropdownTitle, FooterContainer, StyledDropdown } from "./MySearchesModal.styles";
+import {
+  ModalDialog,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  DropdownTitle,
+  FooterContainer,
+  StyledDropdown,
+} from "./MySearchesModal.styles";
 import { Pagination } from "@mui/material";
 import MySearchesItem from "../MySearchesItem/MySearchesItem.component";
 import { DropdownIcon } from "../worldwideDropdown/worldwideDropdown.styles";
 
+const MySearchModal = ({
+  show,
+  handleClose,
+  saveSearches,
+  handleEditSearch,
+  handleDeleteSearch,
+}) => {
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(
+    parseInt((saveSearches.length - 1) / rowsPerPage) + 1
+  );
+  const [searchView, setSearchView] = useState(
+    saveSearches.slice(0, rowsPerPage)
+  );
 
+  const updateSearchView = () => {
+    setSearchView(
+      saveSearches.slice(
+        (currentPage - 1) * rowsPerPage,
+        currentPage * rowsPerPage
+      )
+    );
+  };
 
-const MySearchModal = ({show, handleClose, saveSearches}) =>{
-    const [rowsPerPage, setRowsPerPage] = useState(10)
-    const [totalPages, setTotalPages] = useState(parseInt((saveSearches.length-1) / rowsPerPage)+1)
-    const [searchView, setSearchView] = useState(saveSearches.slice(0,rowsPerPage))
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    setSearchView(
+      saveSearches.slice((newPage - 1) * rowsPerPage, newPage * rowsPerPage)
+    );
+  };
 
-    const handlePageChange = (newPage)=>{
-        setSearchView(saveSearches.slice((newPage-1)*rowsPerPage, (newPage*rowsPerPage)))
-    }
+  const handleRowsPerPageChange = (number) => {
+    setRowsPerPage(number);
+    setTotalPages(parseInt((saveSearches.length - 1) / number) + 1);
+    setSearchView(saveSearches.slice(0, number));
+  };
 
-    const handleRowsPerPageChange = (number)=>{
-        setRowsPerPage(number)
-        setTotalPages(parseInt((saveSearches.length-1) / number)+1)
-        setSearchView(saveSearches.slice(0,number))
-    }
+  const RowsPerPageDropdown = () => (
+    <StyledDropdown
+      title={
+        <DropdownTitle>
+          Rows per page {rowsPerPage}
+          <DropdownIcon src="/dropdown-arrow-svgrepo-com.svg" />
+        </DropdownTitle>
+      }
+      position="right"
+    >
+      {[2, 5, 10, 15].map((number) => (
+        <Dropdown.Item
+          key={number}
+          onClick={() => handleRowsPerPageChange(number)}
+        >
+          {number}
+        </Dropdown.Item>
+      ))}
+    </StyledDropdown>
+  );
+  useEffect(() => {
+    console.log("================saveSearches==================");
+    console.log(saveSearches);
+    console.log("===========================================");
+  }, [saveSearches]);
 
-    const RowsPerPageDropdown = () => (
-        <StyledDropdown title={
-                <DropdownTitle>Rows per page {rowsPerPage}<DropdownIcon src="/dropdown-arrow-svgrepo-com.svg"/></DropdownTitle> 
-        } position="right">
-           {[2, 5, 10, 15].map((number) => (
-            <Dropdown.Item key={number} onClick={() => handleRowsPerPageChange(number)}>{number}</Dropdown.Item>
-           ))}
-        </StyledDropdown>
-    )
+  useEffect(() => {
+    updateSearchView();
+  }, [saveSearches, rowsPerPage]);
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <ModalDialog>
+          <ModalHeader closeButton>
+            <Modal.Title id="example-custom-modal-styling-title">
+              My Searches
+            </Modal.Title>
+          </ModalHeader>
+          <ModalBody>
+            {searchView.map((item) => (
+              <MySearchesItem
+                key={item.id}
+                item={item}
+                handleEditSearch={handleEditSearch}
+                handleDeleteSearch={handleDeleteSearch}
+              />
+            ))}
+            <FooterContainer>
+              <RowsPerPageDropdown />
+              <Pagination
+                count={totalPages}
+                color="primary"
+                onChange={(event, value) => {
+                  handlePageChange(value);
+                }}
+              />
+            </FooterContainer>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalDialog>
+      </Modal>
+    </>
+  );
+};
 
-    return(
-        <>
-            <Modal
-                show={show}
-                onHide={handleClose}
-                aria-labelledby="example-custom-modal-styling-title"
-            >
-                <ModalDialog>
-                <ModalHeader closeButton>
-                    <Modal.Title id="example-custom-modal-styling-title">
-                    My Searches
-                    </Modal.Title>
-                </ModalHeader>
-                <ModalBody>
-                    {searchView.map((item) => (
-                        <MySearchesItem item={item}/>
-                    ))}
-                <FooterContainer>
-                <RowsPerPageDropdown />
-                <Pagination count={totalPages} color="primary" onChange={(event, value)=> {handlePageChange(value)}}/>
-                </FooterContainer>
-                </ModalBody>
-                <ModalFooter>
-                </ModalFooter>
-                </ModalDialog>
-            </Modal>
-        </>
-    )
-}
-
-export default MySearchModal
+export default MySearchModal;
